8aec2076cf72679bd0d0f78265c934dfdb63604a
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-10-25 16:08:00 -0300

    Adicionando um assinante na lista de e-mail

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -6,6 +6,7 @@ use App\Models\EmailList;
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Validation\Rule;
 
 class SubscriberController extends Controller
 {
@@ -26,6 +27,21 @@ class SubscriberController extends Controller
             'search' => $search,
             'showTrash' => $showTrash,
         ]);
+    }
+
+    public function create(EmailList $emailList) {
+        return view('subscribers.create', compact('emailList'));
+    }
+
+    public function store(EmailList $emailList) {
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('subscribers')->where('email_list_id', $emailList->id)],
+        ]); 
+
+        $emailList->subscribers()->create($data);
+
+        return to_route('subscribers.index', $emailList)->with('message', __('Subscriber successfully created!'));
     }
 
     public function destroy(mixed $list, Subscriber $subscriber)
